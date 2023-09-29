08c00b37962d4cd2d6508f698555da55dbd90ba9
JKisley <47765931+JKisley@users.noreply.github.com>
2023-09-29 15:55:49 -0300

    Altereção no código com alguns exemplos, com o básico de informação

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,44 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// [Playground](https://pl.kotl.in/3dTUGvuC)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun calcularTempoTotal(): Int {
+        return conteudos.sumOf { it.duracao }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 30)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 40)
+
+    val formacao = Formacao("Desenvolvimento Kotlin", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    println("Formação: ${formacao.nome}")
+    println("Nível: ${formacao.nivel}")
+    println("Conteúdos:")
+    formacao.conteudos.forEach { println("- ${it.nome} (${it.duracao} minutos)") }
+
+    println("\nPessoas Matriculadas:")
+    formacao.inscritos.forEach { println("- ${it.nome}") }
+
+    println("\nTempo Total da Formação: ${formacao.calcularTempoTotal()} minutos")
 }
+
